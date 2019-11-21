979f47ab4b1136b991d0857eae255402476ecd0c
yue <brand960@foxmail.com>
2019-11-21 12:25:41 +0800

    修改 启动逻辑

--- src/main/java/com/plantserver/PlantserverApplication.java
+++ src/main/java/com/plantserver/PlantserverApplication.java
@@ -1,21 +1,26 @@
 package com.plantserver;
 
 import com.plantserver.service.MqttMsgPublisher;
-import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+//@EnableScheduling
 @SpringBootApplication
 public class PlantserverApplication {
-
-    private static final Logger log = Logger.getLogger(PlantserverApplication.class);
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder().sources(PlantserverApplication.class).web(WebApplicationType.NONE).run(args);
-        MqttMsgPublisher test = new MqttMsgPublisher();
-        test.sendMqtt();
+        SpringApplication.run(PlantserverApplication.class, args);
+        System.out.println("\n"+
+                "  _____  _               _   _ _______    _____ ______ _______      ________ _____  \n" +
+                " |  __ \\| |        /\\   | \\ | |__   __|  / ____|  ____|  __ \\ \\    / /  ____|  __ \\ \n" +
+                " | |__) | |       /  \\  |  \\| |  | |    | (___ | |__  | |__) \\ \\  / /| |__  | |__) |\n" +
+                " |  ___/| |      / /\\ \\ | . ` |  | |     \\___ \\|  __| |  _  / \\ \\/ / |  __| |  _  / \n" +
+                " | |    | |____ / ____ \\| |\\  |  | |     ____) | |____| | \\ \\  \\  /  | |____| | \\ \\ \n" +
+                " |_|    |______/_/    \\_\\_| \\_|  |_|    |_____/|______|_|  \\_\\  \\/   |______|_|  \\_\\");
+//        new SpringApplicationBuilder().sources(PlantserverApplication.class).web(WebApplicationType.NONE).run(args);
+//        MqttMsgPublisher test = new MqttMsgPublisher();
+//        test.sendMqtt();
     }
 }
