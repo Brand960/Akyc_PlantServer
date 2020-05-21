b3da8dddf9530816c7967f395f156bc968048736
yuepeng <brand960@foxmail.com>
2020-05-21 15:21:59 +0800

    添加 断开重连期间不发送

--- akyc_edge_docker/edge_redis.py
+++ akyc_edge_docker/edge_redis.py
@@ -21,8 +21,9 @@ handler.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(handler)
 logger.addHandler(ch)
-f = open('config.yaml')
+f = open('config/config.yaml')
 content = yaml.load(f, Loader=yaml.FullLoader)
+debug = content['debug']
 saferconHost = content['north']['saferconHost']
 raspTopic=content['south']['raspTopic']
 saferconTopic=content['north']['saferconTopic']
@@ -31,6 +32,7 @@ checkInterval=content['south']['checkInterval']
 pool = redis.ConnectionPool(host='redis', port=6379, decode_responses=False)   # host是redis主机，需要redis服务端和客户端都起着 redis默认端口是6379
 r = redis.Redis(connection_pool=pool)
 DEVICE_LIST=[]
+CLOUD_MQTT_CON=False
 
 class AkycCache:
   def __init__(self,r):
@@ -93,42 +95,91 @@ akycCache=AkycCache(r)
 def on_mqtt_connect(client,address,port):
   client.connect(address, port, 60)
   client.loop_start()
-  print(address+" mqtt server connected")
+
+def on_connect_come(client, userdata, flags, rc):
+  _str=str(userdata)
+  logger.info(_str+" connected "+str(rc))
+  if _str == "cloud":
+    global CLOUD_MQTT_CON
+    CLOUD_MQTT_CON=True
 # 消息处理函数
 def on_message_come(client, userdata, msg):
   if(msg.topic and msg.topic == raspTopic):
     akycCache.rpush(msg.payload)
-def on_disconnect_come1(client, userdata, rc):
-  logger.warning("local mqtt disconnected")
-def on_disconnect_come2(client, userdata, rc):
-  logger.warning("cloud mqtt disconnected")
+def on_disconnect_come(client, userdata, rc):
+  _str=str(userdata)
+  logger.info(_str+" disconnected: "+str(rc))
+  if _str == "cloud":
+    global CLOUD_MQTT_CON
+    CLOUD_MQTT_CON=False
+def on_log_come(client, userdata, level, buf):
+  logger.debug(str(userdata)+str(buf))
+
 def run_southBound(n,cache):
   print("current task：", n, " local mqtt")
   mqttClient = mqtt.Client()
-  mqttClient.on_connect = on_mqtt_connect(mqttClient,"mqtt",1883)
-  mqttClient.subscribe(raspTopic, 0)
+  mqttClient.reinitialise("raspberrypi",True,"local")
   mqttClient.on_message = on_message_come # 消息到来处理函数
-  mqttClient.on_disconnect = on_disconnect_come1
+  mqttClient.on_disconnect = on_disconnect_come
+  mqttClient.on_connect = on_connect_come
+  while(True):
+    try:
+      on_mqtt_connect(mqttClient,"mqtt",1883)
+      mqttClient.subscribe(raspTopic, 0)
+      break
+    except Exception as e:
+      print(e)
+      logger.error(e)
+      time.sleep(10)
+      continue
+  if debug:
+    mqttClient.on_log=on_log_come
   while(True):
     pass
 
 def run_northBound(n,cache):
   print("current task：", n, " cloud mqtt")
   mqttClient2 = mqtt.Client()
-  mqttClient2.on_connect = on_mqtt_connect(mqttClient2,saferconHost,1883)
-  mqttClient2.on_disconnect = on_disconnect_come2
+  mqttClient2.reinitialise("raspberrypi",True,"cloud")
+  mqttClient2.on_disconnect = on_disconnect_come
+  mqttClient2.on_connect = on_connect_come
+  if debug:
+    mqttClient2.on_log=on_log_come
+  while(True):
+    try:
+      on_mqtt_connect(mqttClient2,saferconHost,1883)
+      break
+    except Exception as e:
+      print(e)
+      logger.error(e)
+      time.sleep(10)
+      continue
+
   while(True):
     while(cache.llen()>0):
       try:
-        data=akycCache.lpop()
-        mqttClient2.publish(saferconTopic, data, 0)
-        print("- ",time.strftime('%Y.%m.%d/%H:%M:%S',time.localtime(time.time())),\
+        if CLOUD_MQTT_CON:
+          data=akycCache.lpop()
+          MQTTMessageInfo=mqttClient2.publish(saferconTopic, data, 1)
+          # print(MQTTMessageInfo.is_published())
+          print("- ",time.strftime('%Y.%m.%d/%H:%M:%S',time.localtime(time.time())),\
 		"(uid)",int.from_bytes(data[:4],'little'),\
 		"(size/num)",data[6],\
 		"/",data[7],\
 		"(cache length)", akycCache.llen())
+        else:
+           time.sleep(10)
+           print("==",time.strftime('%Y.%m.%d/%H:%M:%S',time.localtime(time.time())),
+                "transpot suspened due to disconnect with cloud mqtt ",
+                "(cache length)", akycCache.llen())
       except Exception as e:
-        akycCache.lpush(data)
+        if data:
+          akycCache.lpush(data)
+          print("+ ",time.strftime('%Y.%m.%d/%H:%M:%S',time.localtime(time.time())),\
+		"(uid)",int.from_bytes(data[:4],'little'),\
+		"(size/num)",data[6],\
+		"/",data[7],\
+		"(cache length)", akycCache.llen())
         logger.error(e)
     pass
 
@@ -137,10 +188,11 @@ def run_checkInterval():
   for element in DEVICE_LIST:
     if not r.exists(element):
       logger.warning(str(element)+" disconnected")
-  print("current task：check device list ok") 
+      DEVICE_LIST.pop(DEVICE_LIST.index(element))
   global timer
   timer = threading.Timer(checkInterval, run_checkInterval)
   timer.start()
+
 if __name__ == '__main__':
   t1 = threading.Thread(target=run_southBound, args=("thread 1",akycCache))
   t2 = threading.Thread(target=run_northBound, args=("thread 2",akycCache))
